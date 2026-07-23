ef8f107f977db3463fb24ed2b77c82da29c4626d
Sravan Kambhampati <sravankambhampati@gmail.com>
2026-07-23 07:12:36 +0530

    Create homepage using Django templates

--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 def home(request):
-    return HttpResponse('<h1> Welcome to expense tracker </h1>')
+    return render(request, "expenses/home.html")
