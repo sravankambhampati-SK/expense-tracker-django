c92be19b751d2b970ea5e32c1580cae773958666
Sravan Kambhampati <sravankambhampati@gmail.com>
2026-07-23 06:52:08 +0530

    Urls and views modified

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse 
+def home(request):
+    return HttpResponse('<h1> Welcome to expense tracker </h1>')
